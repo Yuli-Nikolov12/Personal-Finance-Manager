dacaa5dec71cb156dbf8d03d6020c64c07fbf452
Yuli-Nikolov12 <yuli.nikolov99@gmail.com>
2025-04-02 20:47:02 +0300

    Added ConnetionString to json file, creating classes for the DbContext and installing nugetPackage for using EntityFramework

---
+++ PersonalFinance.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonalFinance.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {}
+    }
+}
