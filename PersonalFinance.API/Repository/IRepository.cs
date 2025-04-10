edc862f7bd2de308776025ba57b4fa92a4311044
Yuli-Nikolov12 <yuli.nikolov99@gmail.com>
2025-04-10 20:48:57 +0300

    Adding the overloading in the Interface of the Repository

--- PersonalFinance.API/Repository/IRepository.cs
+++ PersonalFinance.API/Repository/IRepository.cs
@@ -2,7 +2,9 @@
 {
     public interface IRepository<T> where T: class
     {
+        Task<IEnumerable<T>> GetAllAsync();
         Task<IEnumerable<T>> GetAllAsync(QueryOptions<T> options);
+        Task<T> GetByIdAsync(int id);
         Task<T> GetByIdAsync(int id, QueryOptions<T> options);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
